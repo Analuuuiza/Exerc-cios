d2f871c7aacf1d920c1c9ae3e818b74fc102c703
Analuuuiza <anaedara123@gmail.com>
2023-07-27 15:52:34 -0300

    primeiro commit

---
+++ routes/api.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\ExercicioCincoController;
+use App\Http\Controllers\ExercicioDoisController;
+use App\Http\Controllers\ExercicioQuatroController;
+use App\Http\Controllers\ExercicioSeisController;
+use App\Http\Controllers\ExercicioSeteController;
+use App\Http\Controllers\ExercicioTresController;
+use App\Http\Controllers\ExercicioUmController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::post('exercicio/um', 
+[ExercicioUmController::class, 'multiplicar']);
+
+Route::post('exercicio/dois', 
+[ExercicioDoisController::class, 'retornarMaior']);
+
+Route::get('exercicio/sete', 
+[ExercicioSeteController::class, 'exibirNumeros']);
+
+Route::post('exercicio/quatro', 
+[ExercicioQuatroController::class, 'verificarNumero']);
+
+Route::post('exercicio/tres', 
+[ExercicioTresController::class, 'media']);
+
+Route::post('exercicio/cinco',
+[ExercicioCincoController::class, 'verificar']);
+
+Route::post('exercicio/seis', [ExercicioSeisController::class, 'idade']);
